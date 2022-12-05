cd3f97d09dfa4d429eaf57b5ee5ddf27529f174f
SamuelW523 <91629324+SamuelW523@users.noreply.github.com>
2022-12-05 13:29:46 -0600

    Update PurchaseScreenController.java

--- src/sample/PurchaseScreenController.java
+++ src/sample/PurchaseScreenController.java
@@ -12,5 +12,36 @@ import java.io.File;
 
 public class PurchaseScreenController {
   
+  @FXML
+  private void onPurchaseClicked(javafx.event.ActionEvent actionEvent) throws IOException {
+    
+    Parent currScene = FXMLLoader.load(getClass().getResource("")); // View Times
+    Stage currStage = Main.getStage();
+
+    currStage.setTitle("Available Times");
+    currStage.setScene((new Scene(currScene, 600, 400)));
+    currStage.show();
+
+  }
+  
+  @FXML
+  private void onTimeClicked(javafx.event.ActionEvent actionEvent) throws IOException {
+    
+    Parent currScene = FXMLLoader.load(getClass().getResource("")); // View and Enter Payment Details
+    Stage currStage = Main.getStage();
+
+    currStage.setTitle("");
+    currStage.setScene((new Scene(currScene, 600, 400)));
+    currStage.show();
+
+  }
+  
+  private void onReviewClicked(javafx.event.ActionEvent actionEvent) throws IOException {
+    
+    //this can be done last if need be
+    
+  }
+  
+  
 }
 
