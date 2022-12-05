e8bb52c0547bcb13b907c00604be6d52eec4bdb8
Colby Killian <colbyjack2000@gmail.com>
2022-12-05 12:43:50 -0600

    Added login system, created customer screen

--- src/sample/MovieScreenController.java
+++ src/sample/MovieScreenController.java
@@ -1,16 +1,14 @@
 package sample;
-import java.io.File;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-
 import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MovieScreenController {
     
-    @FMXL private TextField SearchBar;
+    @FXML private TextField SearchBar;
     
 
     @FXML
