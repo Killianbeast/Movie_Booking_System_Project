0fd62821c005c4dec42a4e38f5d9d0c8efd785b8
SamuelW523 <91629324+SamuelW523@users.noreply.github.com>
2022-12-05 13:00:14 -0600

    Create PurchaseScreenController.java

---
+++ src/sample/PurchaseScreenController.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.io.File;
+
+public class PurchaseScreenController {
+  
+}
+
