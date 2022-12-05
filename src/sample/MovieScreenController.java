101bb3ed35c0c9d8236d5385182a837d2611233a
SamuelW523 <91629324+SamuelW523@users.noreply.github.com>
2022-12-04 20:37:07 -0600

    Update MovieScreenController.java

--- src/sample/MovieScreenController.java
+++ src/sample/MovieScreenController.java
@@ -1,4 +1,5 @@
 package sample;
+import java.io.File;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,12 +21,24 @@ public class MovieScreenController {
     @FXML
     private void onSearchClicked(javafx.event.ActionEvent actionEvent) throws IOException {
         
-        string searchfor = SearchBar.getText()
-            
+        String searchfor = SearchBar.getText();
+        
+        searchfor += ".png"; 
+        String imageDirectory = System.getProperty("src/sample/img");
+        File workingFile = new File(imageDirectory);
+        File testfile = new File(workingFile, searchfor);
+        
+        if(!testfile.exists()){
+            System.out.println("Movie Not Found.");
+        }
+        else{
+            //Go to respective purchase purchase screen
+        }
+        
+        
          
         //grab the uhhhhhh thing you just searched for and check to see if the name exists in the image file and then go directly to that purchase screen
-            
-        
+           
      
     
     }
